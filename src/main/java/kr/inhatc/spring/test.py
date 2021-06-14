4b4f0378a9f8978f031b40e25bd8ac48a5b8fd1a
YE-JIMIN <jm567890@naver.com>
2021-06-14 20:03:18 +0900

    게시판/사용자 테이블 틀 조정 및 개발자 조정중

---
+++ src/main/java/kr/inhatc/spring/test.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import kss
+
+import sys
+sys.path.append('C:/Users/USER/Desktop/textrank-master')
+
+from textrank import KeysentenceSummarizer
+
+
+sents = ' '
+inputArgs = sys.argv
+for i in range(1, len(inputArgs)):
+    # i is a number, from 1 to len(inputArgs)-1
+    text = sys.argv[i]
+    sents = sents + ' ' + text
+
+characters = '"!?\n'
+
+for x in range(len(characters)):
+    sents = sents.replace(characters[x],"")
+    
+sts = []
+for sent in kss.split_sentences(sents):
+    sts.append(sent)
+
+summarizer = KeysentenceSummarizer(
+    tokenize = lambda x:x.split(),
+    min_sim = 0.3,
+    verbose = False
+)
+keysents = summarizer.summarize(sts, topk=3)
+for _, _, sent in keysents:
+    print(sent)
